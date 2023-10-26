1d8f5616df3f89a5c20852957d02eb17043ef11c
ElaineLeandro <elainerleandro1@gmail.com>
2023-10-26 09:59:15 -0300

    feat: implementação mascara no campo input e-mail.

--- login/js/script.js
+++ login/js/script.js
@@ -1,2 +1,10 @@
-/** Adcionar o elemento */
+/** Máscara para o campo de e-mail */
 
+const emailInput = document.getElementById('email')
+emailInput.addEventListener("input", function() {
+    let value = this.value;
+    value = value.replace(/\s/g, "") // Remova os espaços em branco
+    this.value = value
+})
+
+console.log(emailInput)
