a5f54bfc2cdab0e6209da2c57e30aa6c687b5856
ElaineLeandro <elainerleandro1@gmail.com>
2023-11-07 11:34:23 -0300

    feat: aplicando a validação campos input no Form.

--- login/js/script.js
+++ login/js/script.js
@@ -1,10 +1,48 @@
-/** Máscara para o campo de e-mail */
+/**Função de validação do formulário login */
+   
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('loginForm')
 
-const emailInput = document.getElementById('email')
-emailInput.addEventListener("input", function() {
-    let value = this.value;
-    value = value.replace(/\s/g, "") // Remova os espaços em branco
-    this.value = value
+    /** Obetém os campos de entrada */
+    const emailInput = document.getElementById('email');
+    const passwordInput  = document.getElementById('password');
+    const emailError = document.getElementById('email-error');
+    const passwordError  = document.getElementById('password-error');
+    const successMessage = document.getElementById('success-message');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        emailError.textContent = '';
+        passwordError.textContent = '';
+        successMessage.textContent = '';
+
+        const email = emailInput.value;
+        const password = passwordInput.value;
+        
+        
+        if(!isValidEmail(email)){
+            emailError.textContent = 'Por favor, insira um e-mail válido.';
+            return;
+        };
+
+        if(!isValidPassword(password)){
+            passwordError.textContent = 'A senha deve conter pelo menos 8 caracteres.'
+        }
+         
+        /** Se todas as validações passarem , exiba uma mensagem de sucesso */
+        successMessage.textContent = 'Login bem-sucedido! Agora você pode redirecionar para a próxima página ou executar a lógica de login adequada.'
+    });
+
+    function isValidEmail(email){
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email)
+    };
+
+    function isValidPassword(password){
+        return password.length >= 8
+    };
+    
 })
+  
+    
 
-console.log(emailInput)
